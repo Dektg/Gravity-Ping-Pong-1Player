a275f4f2acb8161f1a91fcb1d0559908915ec4e8
Dektg <56469979+Dektg@users.noreply.github.com>
2020-01-10 12:04:25 +1000

    Добавил ракетку и её материальность

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -3,6 +3,10 @@ import  turtle, random
 window = turtle.Screen()
 window.bgcolor('black')
 window.setup(width=1.0, height=1.0)
+window.tracer(2)
+
+space_rocket = 80
+
 
 border = turtle.Turtle()
 border.color('white')
@@ -28,17 +32,51 @@ ball.shape("circle")
 ball.color("black")
 
 ball.speedY = -1
-ball.speedX = random.choice([-8, -7, -6, -5, 5, 6, 7, 8])
+ball.speedX = random.choice([-5,-4,-3,3,4, 5])
+
+# РАКЕТКА
+rocket = turtle.Turtle()
+rocket.color("black")
+rocket.shape("square")
+rocket.shapesize(stretch_len=5, stretch_wid=1)
+    # Не оставляет следов
+rocket.penup()
+rocket.goto(0, -250)
+
+# УПРАВЛЕНИЕ РАКЕТКИ
+def move_Right():
+    x = rocket.xcor() + space_rocket
+    if x > 450:
+        x = 450
+    rocket.setx(x)
 
 
+def move_Left():
+    x = rocket.xcor() - space_rocket
+    if x < -450:
+        x = -450
+    rocket.setx(x)
 
-GRAVITY = 0.17
+
+window.listen()
+window.onkeypress(move_Right, "d")
+window.onkeypress(move_Left, "a")
+window.onkeypress(move_Right, "Right")
+window.onkeypress(move_Left, "Left")
+
+
+GRAVITY = 0.07
 
 while True:
+    if ball.ycor() >= rocket.ycor() - 10 and ball.ycor() <= rocket.ycor() + 10 \
+            and ball.xcor() >= rocket.xcor() - 50 and ball.xcor() <= rocket.xcor() + 50:
+        ball.speedY = -ball.speedY
+
     ball.speedY = ball.speedY - GRAVITY
     ball.goto(ball.xcor() + ball.speedX, ball.ycor() + ball.speedY)
     if ball.ycor() <= -290:
         ball.speedY = -ball.speedY
+
     if ball.xcor() <= -390 or ball.xcor() >= 390:
         ball.speedX = -ball.speedX
 window.mainloop()
